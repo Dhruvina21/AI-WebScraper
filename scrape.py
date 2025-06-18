d62212f16cd6ecc57327c6bfcf22fff7353df3f9
Dhruvina21 <dhruvina21@gmail.com>
2025-06-18 16:28:21 -0700

    First commit

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import service
+import time
 
 def scrape_website(website):
     print("Launching chrome browser...")
@@ -7,4 +8,17 @@ def scrape_website(website):
     chrome_driver_path = ""
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    try:
+        #use our webdriver to get to this specific website
+        driver.get(website)
+        print("Page loaded...")
+        html = driver.page_source
+        time.sleep(10)
+
+        return html
+    finally:
+        driver.quit()
+
+
     
