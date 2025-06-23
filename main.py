dfdb2a96d91636156a78a26d040c1afbce6fdc66
Dhruvina21 <dhruvina21@gmail.com>
2025-06-23 16:27:03 -0700

    Calling the function in the main file + expanding the view for content

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from scrape import scrape_website
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    clean_body_content,
+    extract_body_content,
+)
 
 st.title("Webscraper AI") #adds a title 
 url = st.text_input("Enter your website URL that needs a scrapping: ") #a text input
@@ -7,7 +13,17 @@ url = st.text_input("Enter your website URL that needs a scrapping: ") #a text i
 #button if clicked on Scrape site to pass
 if st.button("Scrape Site"):
     st.write("Scrapping the website")
+
     result = scrape_website(url)
-    print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+    #expander text box to allow us view more content
+    with st.expander("View DOM content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
+
+    
 
 
